fd3a45d0e6db3687ca29301a5c758e1cee7d3040
Aaron Tournoud <atournoudfrcu@gmail.com>
2024-12-20 15:10:07 -0300

    asda

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { AuthService } from '@abp/ng.core';
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { ConfigStateService  } from '@abp/ng.core';
 import { SerieDto, SerieService } from '@proxy/series';
 import { register } from "swiper/element/bundle";
-import Swiper from 'swiper';
+
+
 
 register()
 
@@ -12,8 +13,8 @@ register()
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  
   public imdbIds = ["tt5753856","tt5607976","tt11912196","tt19231492","tt4159076","tt0204993"];
 
   public series = [] as SerieDto[];
@@ -24,6 +25,12 @@ export class HomeComponent {
 
   constructor(private authService: AuthService, private config: ConfigStateService, private serieService: SerieService) {
   }
+
+  ngOnInit() {
+    this.getSeriesList(this.imdbIds)
+  }
+
+  
 
   login() {
     this.authService.navigateToLogin();
@@ -54,44 +61,7 @@ export class HomeComponent {
       this.getSerieImdbId(id)})
   }
 
-  async  fetchProductos(){
-    await this.getSeriesList(this.imdbIds);
-    console.log(this.series)
-    const swiperWrapper = document.getElementById('swiper-wrapper');
-    this.series.forEach(serie => {
-      console.log(serie)
-      const slide = document.createElement('div');
-                      slide.classList.add('swiper-slide');
   
-                      slide.innerHTML = 
-                          `<div>
-                              <h1>${serie.title}</h1>
-                          </div>
-                          <img src="${serie.poster} alt="jempatv poster">`
-                      ;
-                      swiperWrapper.appendChild(slide)
-      });
+  }
+
   
-      new Swiper (".mySwiper", {
-        effect: "coverflow",
-        centeredSlides: true,
-        slidesPerView: "auto",
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,},
-        coverflowEffect: {
-            depth:500,
-            modifier:1,
-            slideShadows: true,
-            rotate:0,
-            stretch:0
-        }
-  
-        ,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        }})
-    }
-}
--- angular/src/app/home/home.module.ts
+++ angular/src/app/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { PageModule } from '@abp/ng.components/page';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
@@ -8,5 +8,6 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [HomeComponent],
   imports: [SharedModule, HomeRoutingModule, PageModule, CommonModule],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomeModule {}
