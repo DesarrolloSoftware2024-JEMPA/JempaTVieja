e8895a6f41c75a9ac586737dfc1cd56250dfb44f
Mateo <mateo.n@outlook.com>
2024-12-20 12:13:25 -0300

    swiper slide!

--- angular/src/app/home/home.component.ts
+++ angular/src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import { AuthService } from '@abp/ng.core';
 import { Component } from '@angular/core';
-import { ConfigStateService  } from '@abp/ng.core';
+import { ConfigStateService } from '@abp/ng.core';
 import { SerieDto, SerieService } from '@proxy/series';
-import { register } from "swiper/element/bundle";
-import Swiper from 'swiper';
+import { register } from 'swiper/element/bundle';
 
-register()
+register();
 
 @Component({
   selector: 'app-home',
@@ -13,85 +12,47 @@ register()
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-
-  public imdbIds = ["tt5753856","tt5607976","tt11912196","tt19231492","tt4159076","tt0204993"];
+  public imdbIds = ['tt5753856', 'tt5607976', 'tt11912196', 'tt19231492', 'tt4159076', 'tt0204993'];
 
   public series = [] as SerieDto[];
 
   get hasLoggedIn(): boolean {
-    return this.authService.isAuthenticated
+    return this.authService.isAuthenticated;
   }
 
-  constructor(private authService: AuthService, private config: ConfigStateService, private serieService: SerieService) {
-  }
+  constructor(
+    private authService: AuthService,
+    private config: ConfigStateService,
+    private serieService: SerieService
+  ) {}
 
   login() {
     this.authService.navigateToLogin();
   }
 
-  getCurrentUserName(){
-    const currentUserName = this.config.getOne("currentUser").userName;
+  getCurrentUserName() {
+    const currentUserName = this.config.getOne('currentUser').userName;
     return currentUserName;
   }
 
-  getCurrentUserId(){
-    const currentUserId = this.config.getOne("currentUser").id;
+  getCurrentUserId() {
+    const currentUserId = this.config.getOne('currentUser').id;
     console.log(currentUserId);
     return currentUserId;
   }
-  
 
-  getSerieImdbId(imdbId: string) {
-    var serieDto: SerieDto;
-    this.serieService.searchImdbId(imdbId).subscribe(res => {
-      serieDto = res;
-      this.series.push(serieDto);
+  getCarouselItem(imdbId: string) {
+    this.serieService.searchImdbId(imdbId).subscribe(serie => {
+      const swiperWrapper = document.getElementById(serie.imdbID);
+      const slide = document.createElement('div');
+      slide.innerHTML = `<img src=${serie.poster} alt=${serie.title + ' poster'} ></img>`;
+      swiperWrapper.appendChild(slide);
     });
   }
 
-  getSeriesList(imdbId:string[]){
-    imdbId.forEach(id => {
-      this.getSerieImdbId(id)})
+  getCarousel(imdbIds: string[]) {
+    imdbIds.forEach(id => {
+      this.getCarouselItem(id);
+    });
   }
-
-  async  fetchProductos(){
-    await this.getSeriesList(this.imdbIds);
-    console.log(this.series)
-    const swiperWrapper = document.getElementById('swiper-wrapper');
-    this.series.forEach(serie => {
-      console.log(serie)
-      const slide = document.createElement('div');
-                      slide.classList.add('swiper-slide');
-  
-                      slide.innerHTML = 
-                          `<div>
-                              <h1>${serie.title}</h1>
-                          </div>
-                          <img src="${serie.poster} alt="jempatv poster">`
-                      ;
-                      swiperWrapper.appendChild(slide)
-      });
-  
-      new Swiper (".mySwiper", {
-        effect: "coverflow",
-        centeredSlides: true,
-        slidesPerView: "auto",
-        loop: true,
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true,},
-        coverflowEffect: {
-            depth:500,
-            modifier:1,
-            slideShadows: true,
-            rotate:0,
-            stretch:0
-        }
-  
-        ,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        }})
-    }
 }
--- angular/src/app/home/home.module.ts
+++ angular/src/app/home/home.module.ts
@@ -4,6 +4,8 @@ import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { CommonModule } from '@angular/common';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+
 
 @NgModule({
   declarations: [HomeComponent],
